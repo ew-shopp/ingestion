0b5dd9eb8691619f45c6e21a6a904bcc6ee37b69
Volker Hoffmann <volker@cheleb.net>
2017-11-16 06:59:07 +0100

    fix: '--detach' instead of '--detach true'

--- service_02_transform.sh
+++ service_02_transform.sh
@@ -8,7 +8,7 @@ workdir=${basedir}/work
 DOCKER_SERVICE="docker service create \
     --replicas 1 \
     --user `id --user`:`id --group` \
-    --detach true \
+    --detach \
     --limit-cpu 1 \
     --mount type=bind,source=${indir},destination=/in \
     --mount type=bind,source=${workdir},destination=/work \
